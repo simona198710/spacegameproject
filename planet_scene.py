f65676c2df3943bab06b852cc732f783b1ec63d8
Simon <34691905+simona198710@users.noreply.github.com>
2023-10-19 13:53:11 +0200

    Update planet_scene.py

--- planet_scene.py
+++ planet_scene.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+from noise import pnoise2
 
 # Initialize Pygame
 pygame.init()
@@ -14,15 +15,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Exploration Game")
 
 # Generate a random planet surface
-def generate_planet_surface(width, height):
+def generate_planet_surface(width, height, scale=15, octaves=6, persistence=0.5, lacunarity=2.0):
     surface = pygame.Surface((width, height))
     for x in range(width):
         for y in range(height):
-            color = (0, 100, 0)  # Green for grass
-            if random.random() < 0.1:
+            nx = x/width - 0.5
+            ny = y/height - 0.5
+            value = pnoise2(nx*scale, ny*scale, octaves=octaves, persistence=persistence, lacunarity=lacunarity, repeatx=1024, repeaty=1024, base=42)
+            if value > 0.1:
+                color = (0, 100, 0)  # Green for grass
+            elif value > -0.1:
                 color = (139, 69, 19)  # Brown for dirt
-            if random.random() < 0.02:
-                color = (192, 192, 192)  # Gray for rocks
+            else:
+                color = (64, 164, 223)  # Blue for water
             surface.set_at((x, y), color)
     return surface
 
